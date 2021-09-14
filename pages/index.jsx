a3bdeace2d469b9055f944703fc4b97fa5ec8cbd
Felix <68397506+feluxerich@users.noreply.github.com>
2021-09-14 08:52:56 +0200

    removed connect github button

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,6 @@
 function HomePage() {
     return (
         <div className="w-full text-center mt-56">
-            <a href="/api/github/oauth/authorize">
-                <button className="bg-gray-800 p-5 rounded-lg">
-                    Connect GitHub
-                </button>
-            </a>
         </div>
     )
 }
