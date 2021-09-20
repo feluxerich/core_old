fc418d01200dfac65512e32ca329b23f911ef486
play.js <git@gituser.com>
2021-09-20 23:36:56 +0200

    removed github oauth

--- pages/api/github/_oauth/authorize.js
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/dist/server/api-utils";
-
-async function authorize(_req, res) {
-    return redirect(res, 302, `https://github.com/login/oauth/authorize?client_id=${process.env.GH_CLIENT_ID}`)
-}
-
-export default authorize;
--- pages/api/github/_oauth/connect.js
+++
@@ -1,5 +0,0 @@
-async function connect(req, res) {
-    console.log(req.body);
-}
-
-export default connect;
