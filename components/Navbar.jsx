7dd50669574913e0ca30f196cc4e20ebc2aa5cff
Felix <felix.waidler@web.de>
2021-10-07 14:43:09 +0200

    fixed margin in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,11 @@
-import { useEffect } from "react";
-import jscookie from "jscookie";
 import Link from "next/link";
 
 function Navbar() {
   return (
     <nav className="bg-gray-800 h-14 flex flex-row items-center text-white">
-      <div className="absolute left-6 flex flex-row">
-        <p className="text-xl">core</p>
-        <Link href="/" passHref className="ml-6">
+      <div className="absolute flex flex-row">
+        <p className="text-xl mx-6">core</p>
+        <Link href="/" passHref>
           <button className="bg-gray-900 px-3 py-1 rounded-md">Home</button>
         </Link>
         <Link href="/minecraft/hypixel" passHref className="ml-3">
